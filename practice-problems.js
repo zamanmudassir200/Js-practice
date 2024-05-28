6ac3e3a09e8b8cacc7bea903d6dbf8d3c963d7a9
Muhammad Mudassir Zaman <127381696+zamanmudassir200@users.noreply.github.com>
2024-05-28 08:40:09 +0500

    add practice problems

---
+++ practice-problems.js
@@ -0,0 +1,34 @@
+// // Number divisible by 10? good :bad
+// let num = 35;
+// // if (num % 10 === 0) {
+// //   console.log("good");
+// // } else {
+// //   console.log("bad");
+// // }
+
+// console.log(num % 10 == 0 ? "good" : "bad");
+
+// // PROBLEM 2
+// let username = prompt("Enter your name");
+// let age = prompt("Enter your age");
+// console.log(`${username} is ${age} years old`);
+
+// PROBLEM 3
+let quarter = 1;
+
+switch (quarter) {
+  case 1:
+    console.log("jan, feb, mar");
+    break;
+  case 2:
+    console.log("apr, may,jun");
+    break;
+  case 3:
+    console.log("jul,aug ,sep");
+    break;
+  case 4:
+    console.log("oct,nov,dec");
+    break;
+  default:
+    console.log("invalid quarter");
+}

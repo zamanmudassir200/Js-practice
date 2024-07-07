7153631b9d1bd87c8e5e677cf4f417bcfe815886
Muhammad Mudassir Zaman <127381696+zamanmudassir200@users.noreply.github.com>
2024-07-08 02:41:59 +0500

    final commit

--- problem-question/index.js
+++ problem-question/index.js
@@ -68,3 +68,20 @@
 // };
 
 // isPalindrome("level");
+
+//? Problem: Write a function fizzBuzz that prints numbers from 1 to 100. But for multiples of 3, print "Fizz" instead of the number, and for the multiples of 5, print "Buzz". For numbers which are multiples of both 3 and 5, print "FizzBuzz".
+
+const fizzBuzz = () => {
+  for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else {
+      console.log(i);
+    }
+  }
+};
+fizzBuzz();
